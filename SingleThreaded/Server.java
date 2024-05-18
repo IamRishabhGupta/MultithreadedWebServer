01ba2ad3537eec80d48002aa91356c1671fe4bda
Rishabh Gupta <rishabh9aug@gmail.com>
2024-05-18 17:23:56 +0530

    port changed to 8010

--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -9,7 +9,7 @@ import java.net.UnknownHostException;
 public class Server {
 
     public void run() throws IOException, UnknownHostException{
-        int port = 8080;
+        int port = 8010;
         ServerSocket socket = new ServerSocket(port);
         socket.setSoTimeout(20000);
         while(true){
